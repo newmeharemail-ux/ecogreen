59321e6511ddf31ad263bfac665d9b55bced0007
Abdurrehman <newmeharemail@gmail.com>
2026-07-04 03:48:24 +0500

    Fix blank page - add basename to BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Reviews from './pages/Reviews';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/ecogreen">
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
