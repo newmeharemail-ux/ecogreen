14ca610e59085348723f2f872d8a6aa4073e14e2
newmeharemail-ux <newmeharemail@gmail.com>
2026-08-15 07:45:45 +0500

    Change BrowserRouter basename from '/ecogreen' to '/'

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Reviews from './pages/Reviews';
 
 export default function App() {
   return (
-    <BrowserRouter basename="/ecogreen">
+    <BrowserRouter basename="/">
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
